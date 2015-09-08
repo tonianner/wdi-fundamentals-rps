5c27c78e60efa7b26af145be2cb9c0f898c3c7f8
toni <tonianne.romero@gmail.com>
2015-09-09 00:17:16 +0800

    updated final

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -95,6 +95,7 @@ function getWinner(playerMove,computerMove) {
             winner = 'wrong value';
         }
     }
+
     return winner;
 } 
 
@@ -103,13 +104,14 @@ getWinner();
 
 
 function playToFive() {
-  console.log('Let\'s play Rock Paper Scissors' + '\n');
+  console.log('Let\'s play Rock Paper Scissors\n');
   var playerWins = 0;
   var computerWins = 0;
+  var wins = 5;
   
   // This function should continue to play Rock Paper Scissors until either the player or the computer has won five times.
   var winner = getWinner(playerWins, computerWins);
-    while (playerWins < 2 && computerWins < 2) {
+    while (playerWins < wins && computerWins < wins) {
         var computerMove = getComputerMove();
         var playerMove = getPlayerMove();
 
@@ -121,19 +123,28 @@ function playToFive() {
             } else if (winner === 'tie') {
                 playerWins += 0;
                 computerWins += 0;
-            } 
+                console.log('\n IT\'S A TIE, TRY AGAIN \n');
+            }else if (playerMove != 'rock' || playerMove != 'scissors' || playerMove != 'paper') {
+        console.log('\n ERROR! Wrong Input \n')
+            }
   
 // After each 'round', display some text in the console indicating who played what, who won, and what the current scoreboard looks like.
 
-    console.log('Player chose: ' + playerMove);
-    console.log('Computer chose: ' + computerMove);
-    console.log('The score is currently Player: ' + playerWins + ', Computer: ' + computerWins +  '\n');
- 
-}
-    console.log('With the score as Player: ' + playerWins + ', Computer: ' + computerWins +  '\n' + 'THE WINNER IS ' + winner);
-    return [playerWins,computerWins];
+
+//INSERT if statement for log
+    console.log('Player chose: ' + playerMove + '\n'
+            + 'Computer chose: ' + computerMove + '\n'
+            + 'Score: Player ' + playerWins 
+            + ', Computer ' +  computerWins +  '\n');
+    
 }
 
+ console.log('Total Score: Player ' + playerWins 
+            + ', Computer: ' + computerWins +  '\n' 
+            + 'THE WINNER IS \'' + winner + '\''
+            +'\n CONGRATULATIONS!');
+    return [playerWins,computerWins];
+}
 //call function
 playToFive();
 
