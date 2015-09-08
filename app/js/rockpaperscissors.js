28820629ef249a74093ca0274850e6642a32f3b5
toni <tonianne.romero@gmail.com>
2015-09-08 17:33:17 +0800

    updated final version

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -1,10 +1,10 @@
 ////////////////////////////////////////////////
 /*   Provided Code - Please Don't Edit   */
 ////////////////////////////////////////////////
-'use strict';
+//'use strict';
 
 function getInput() {
-    console.log("Please choose either 'rock', 'paper', or 'scissors'.")
+    console.log("Please choose either 'rock', 'paper', or 'scissors'." + '\n');
     return prompt();
 }
 function randomPlay() {
@@ -21,32 +21,38 @@ function randomPlay() {
 /*           Write Your Code Below            */
 ////////////////////////////////////////////////
 
+
+ // Write an expression that operates on a variable called `move`
+// If a `move` has a value, your expression should evaluate to that value.
+// However, if `move` is not specified / is null, your expression should equal `getInput()`.
+
 function getPlayerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `getInput()`.
     return move || getInput();
 }
 
+// Write an expression that operates on a variable called `move`
+// If a `move` has a value, your expression should evaluate to that value.
+// However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
+    
 function getComputerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
     return move || randomPlay();
 }
 
 //we might write the expression x = x || 10;. If x has some 'truthy' value, the OR operator will evaluate to x, so it would be as if we wrote x = x. However, if x were null, the OR operator would evaluate to 10 (because null is 'falsey'). It's as if we've said "If x doesn't already have a value assigned, set it equal to 10". For that reason, this kind of operation is often called 'conditional assignment'.
-//getPlayerMove('rock'); to check if expression is correct
 
+//call function
+getPlayerMove('rock');
+
+
+
+// Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
+// Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
+// The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
+//Use control flow to determine which value gets returned. HINT: the possible outcomes are either s'player' (if the player wins), 'computer' (if the computer wins), or 'tie' (if it's a draw).
 
 function getWinner(playerMove,computerMove) {
     var winner;
-    // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
-    // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
-    // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    // Use control flow to determine which value gets returned. HINT: the possible outcomes are either 'player' (if the player wins), 'computer' (if the computer wins), or 'tie' (if it's a draw).
-    /* YOUR CODE HERE */
-if (playerMove === "rock") 
+    if (playerMove === "rock") 
     {switch (computerMove) 
         {case 'paper':
             winner = "computer";
@@ -92,15 +98,44 @@ if (playerMove === "rock")
     return winner;
 } 
 
-//PlayerMove('player input', 'computerinput'); to check if expressions is correct
+//call function
+getWinner();
 
 
 function playToFive() {
-    console.log("Let's play Rock, Paper, Scissors");
-    var playerWins = 0;
-    var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
-    return [playerWins, computerWins];
+  console.log('Let\'s play Rock Paper Scissors' + '\n');
+  var playerWins = 0;
+  var computerWins = 0;
+  
+  // This function should continue to play Rock Paper Scissors until either the player or the computer has won five times.
+  var winner = getWinner(playerWins, computerWins);
+    while (playerWins < 2 && computerWins < 2) {
+        var computerMove = getComputerMove();
+        var playerMove = getPlayerMove();
+
+         winner = getWinner(playerMove, computerMove);
+             if (winner === 'player') {
+                playerWins ++;
+            } else if (winner === 'computer') {
+                computerWins ++;
+            } else if (winner === 'tie') {
+                playerWins += 0;
+                computerWins += 0;
+            } 
+  
+// After each 'round', display some text in the console indicating who played what, who won, and what the current scoreboard looks like.
+
+    console.log('Player chose: ' + playerMove);
+    console.log('Computer chose: ' + computerMove);
+    console.log('The score is currently Player: ' + playerWins + ', Computer: ' + computerWins +  '\n');
+ 
 }
+    console.log('With the score as Player: ' + playerWins + ', Computer: ' + computerWins +  '\n' + 'THE WINNER IS ' + winner);
+    return [playerWins,computerWins];
+}
+
+//call function
+playToFive();
+
+
 
